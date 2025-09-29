98be97961431f57ebd869fcfdf8f0f26349f89ac
Iamhan-e <h2anaschalew@gmail.com>
2025-09-29 16:19:30 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config';
+import cookieParser from 'cookie-parser';
+
+const app= express()
+const port= process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({Credential:true}));
+
+app.get('/', (req, res)=>{
+    res.send("API working")
+})
+
+app.listen(port, ()=>{
+    console.log(`server started at port ${port}`)
+})
