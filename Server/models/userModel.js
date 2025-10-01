155103b00619d171aea929d823662d1f34072d96
Iamhan-e <h2anaschalew@gmail.com>
2025-10-01 20:24:06 +0300

    Refactor MongoDB connection and add user model schema

---
+++ Server/models/userModel.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const userSchema= new mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    verifyOtp: {type: String, default: ''},
+    verifyOtpExpiray: {type: Number, default: 0},
+    isAccountValid:{type: Boolean, defalut: false},
+    resetOtp: {type: String, default: ''},
+    resetOtpExpiry: {type: Number, default: 0},
+})
+
+const userModel= mongoose.model.user || mongoose.model('user', userSchema)
+
+export default userModel
