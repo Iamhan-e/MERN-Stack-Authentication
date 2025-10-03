d2dd83a019717946ed6d628c4375a7a650afe08f
Iamhan-e <h2anaschalew@gmail.com>
2025-10-03 18:40:13 +0300

    Add logout functionality to clear user session and cookies

--- Server/controller/auth.js
+++ Server/controller/auth.js
@@ -76,6 +76,19 @@ export const login= async(req, res) =>{
         return res.json({success: false, message: error.message})
     }
 
+}
 
+export const logout= (req, res)=>{
+    try {
 
+        res.clearCookie('token', {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: process.env.NODE_ENV === 'production'? 'none': 'strict',
+                maxAge: 7 * 24 * 60 * 60 * 1000 ,
+            })
+        return res.json({success:true, message: 'User has successfully logged out'})
+    } catch (error) {
+        return res.json({success: false, message: error.message})
+    }
 }
